3d4c7546df43d11df8cb188898360092ab50b123
Andrija Petrovic <andrija.hers@gmail.com>
2014-04-24 16:21:25 +0200

    port 80 after all

--- config/env/all.js
+++ config/env/all.js
@@ -3,7 +3,7 @@ rootPath = path.normalize(__dirname + '/../..');
 
 module.exports = {
 	root: rootPath,
-	//port: process.env.PORT || 80,
-	port: process.env.PORT || 16010,
+	port: process.env.PORT || 80,
+	//port: process.env.PORT || 16010,
     db: process.env.MONGOHQ_URL    
 }
