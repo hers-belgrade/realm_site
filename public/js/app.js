b9d63bcd0d4db2358288cffe6e182529492aea2d
Veljko Popovic <veljko@888.rs>
2014-07-22 17:10:07 +0200

    removing ngFitText

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,8 @@
 angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'angularFileUpload', 'mean.ui', 'mean.system', 'mean.avatars', 'mean.bots', 'mean.players','HERS']);
 
 angular.module('mean.ui', []);
-angular.module('mean.ui', ['ngFitText']);
-angular.module('mean.system', ['ngFitText']);
+angular.module('mean.ui', []);
+angular.module('mean.system', []);
 angular.module('mean.avatars', []);
 angular.module('mean.bots', ['ngGrid']);
 angular.module('mean.players', ['ngGrid']);
